24f5413c5a2efeb67b72ee81cc84554b093fef31
sidharampatil1999 <sidharampatil2@gmail.com>
2024-07-16 10:51:57 +0530

    first commit11

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  
+  constructor(private titleService: Title) {
+    
+  }
+  ngOnInit(): void {
+    this.titleService.setTitle("ATS");
+  }
+
+}
